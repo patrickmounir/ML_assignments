5614718e7d78421d4a5803ee7193da8d27663d1a
Patrick Attia <Patrick Attia>
2017-11-25 19:51:52 +0200

    Final Assignment the one submitted

--- Assignment3/PredictionsAndPrecisionPCA.py
+++ Assignment3/PredictionsAndPrecisionPCA.py
@@ -3,6 +3,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import precision_score
 from sklearn.svm import SVC
+from sklearn.decomposition import PCA
 
 
 X_train = np.loadtxt("X_train.txt", dtype=float)
@@ -16,19 +17,24 @@ Y_train = np.loadtxt("y_train.txt", dtype=int)
 
 Y_test = np.loadtxt("y_test.txt", dtype=int)
 
+n = [5,50,200,500]
+pca = PCA(n_components=n[0])
+pca.fit(X_train)
+X_pca = pca.transform(X_train)
+X_pcaTest = pca.transform(X_test)
 classifier = MLPClassifier(solver='adam', random_state=101,
     hidden_layer_sizes=(500,),max_iter=10000,alpha=0.001,activation='tanh',batch_size=1,
     learning_rate='adaptive',learning_rate_init=0.0001, validation_fraction=0.1,early_stopping=True,shuffle=False, verbose=True)
-classifier.fit(X_train, Y_train)
+classifier.fit(X_pca, Y_train)
 print(Y_test)
-predictions = classifier.predict(X_test)
+predictions = classifier.predict(X_pcaTest)
 print(predictions)
 print(precision_score(Y_test, predictions, average='micro'))
 
-svc = SVC(C=1000, gamma=0.001, random_state=101, verbose=True)
+svc = SVC(C=1000, gamma=0.001, random_state=101,  verbose=True)
 
-svc.fit(X_train, Y_train)
+svc.fit(X_pca, Y_train)
 print(Y_test)
-predictions = svc.predict(X_test)
+predictions = svc.predict(X_pcaTest)
 print(predictions)
 print(precision_score(Y_test, predictions, average='micro'))
--- Assignment3/assignment.py
+++ Assignment3/assignment.py
@@ -18,22 +18,22 @@ Y_train = np.loadtxt("y_train.txt", dtype=int)
 
 Y_test = np.loadtxt("y_test.txt", dtype=int)
 
-param_range_HL =  [x for x in itertools.product((300,400,500,600,700),repeat=1)] + [x for x in itertools.product((300,400,500,600,700),repeat=2)]
 if __name__ == '__main__':
-    # train_scores,valid_scores = validation_curve(MLPClassifier(random_state=101),X_train,Y_train,'hidden_layer_sizes',param_range_HL,cv=3,verbose=True,n_jobs=-1)
-    #
-    # np.savetxt('MLPTrainScore.txt', train_scores)
-    # np.savetxt('MLPValidScore.txt', valid_scores)
-    # train_scores_mean = np.mean(train_scores, axis=1)
-    # train_scores_std = np.std(train_scores, axis=1)
+    param_range_HL =  [x for x in itertools.product((300,400,500,600,700),repeat=1)] + [x for x in itertools.product((300,400,500,600,700),repeat=2)]
+    train_scores,valid_scores = validation_curve(MLPClassifier(random_state=101),X_train,Y_train,'hidden_layer_sizes',param_range_HL,cv=3,verbose=True,n_jobs=-1)
+    np.savetxt('MLPTrainScore.txt', train_scores)
+    np.savetxt('MLPValidScore.txt', valid_scores)
+    
     param_range_alpha = [0.00001,0.001,0.1,1,10]
     train_scores,valid_scores = validation_curve(MLPClassifier(random_state=101,hidden_layer_sizes=(500,)),X_train,Y_train,'alpha',param_range_alpha,cv=3,verbose=True,n_jobs=-1)
     np.savetxt('MLPTrainScoreAlpha.txt', train_scores)
     np.savetxt('MLPValidScoreAlpha.txt', valid_scores)
-    # param_range_gamma = [1,0.1,0.01,0.001,0.0001]
-    # train_scores,valid_scores = validation_curve(SVC(random_state=101),X_train,Y_train,'gamma',param_range_gamma,cv=3,verbose=True,n_jobs=-1)
-    # np.savetxt('SVCTrainScoreGamma.txt', train_scores)
-    # np.savetxt('SVCValidScoreGamma.txt', valid_scores)
+
+    param_range_gamma = [1,0.1,0.01,0.001,0.0001]
+    train_scores,valid_scores = validation_curve(SVC(random_state=101),X_train,Y_train,'gamma',param_range_gamma,cv=3,verbose=True,n_jobs=-1)
+    np.savetxt('SVCTrainScoreGamma.txt', train_scores)
+    np.savetxt('SVCValidScoreGamma.txt', valid_scores)
+
     param_range_C = [0.1,1,10,100,1000]
     train_scores,valid_scores = validation_curve(SVC(random_state=101,gamma=0.001),X_train,Y_train,'C',param_range_C,cv=3,verbose=True,n_jobs=-1)
     np.savetxt('SVCTrainScoreC.txt', train_scores)
